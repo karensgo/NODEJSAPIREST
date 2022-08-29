8d4b5c161761cba5f4b1f02d1707b12ef710cc42
Karen Ochoa <67116396+karensgo@users.noreply.github.com>
2022-08-28 21:44:33 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,5 @@
 const { Router} = require('express');
 const router = Router();
-
 const { getTasks, createTask, getTaskById, updateTask, deleteTask} = require ('../controllers/index.controller')
 
 router.get('/tasks', getTasks)
